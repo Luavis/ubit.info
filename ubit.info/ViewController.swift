9b6a480d58c7debba9cbe76e90c4d078b335f295
Luavis <luaviskang@gmail.com>
2015-03-07 19:25:56 +0900

    Initial Commit

---
+++ ubit.info/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ubit.info
+//
+//  Created by 강성일 on 3/7/15.
+//  Copyright (c) 2015 Luavis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
